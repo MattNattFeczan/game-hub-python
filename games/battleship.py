b3de6d9426dc513805ff96b5d2a6266d24d48f49
PikaPot <ZoltyNaelektryzowanyWiewor@proton.me>
2025-12-01 23:03:56 +0100

    trying out with dragging

--- games/battleship.py
+++ games/battleship.py
@@ -4,12 +4,14 @@ import sys
 pygame.init()
 FPS = 60
 CLOCK = pygame.time.Clock()
-WIDTH, HEIGHT = 1080, 1440
+WIDTH, HEIGHT = 1180, 1440
 SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
 SCREEN.set_alpha(None)
+pygame.display.set_caption("Battleship")
 
 enemy_tiles = []
 your_tiles = []
+ships = []
 
 class tile():
     def __init__(self, x, y, tile_map):
@@ -47,6 +49,23 @@ class tile():
         SCREEN.blit(self.surface, self.body)
         pygame.draw.rect(SCREEN, (0, 0, 0), [self.x+3, self.y+3, self.width-6, self.height-6], width=3)
 
+class ship():
+    def __init__(self, x, y, width, height):
+        self.body = pygame.Rect(x, y, width, height)
+        self.color = (255, 255, 255)
+        ships.append(self)
+    def draw(self):
+        pygame.draw.rect(SCREEN, self.color, self.body)
+
+
+x = 950
+y = 500
+z = 1
+for i in range(0, 7):
+    ship(x, y, z*38, 38)
+    if i == 2 or i == 5:
+        z +=2
+    y += 80
     
 x = 0
 y = 44      
@@ -67,12 +86,31 @@ for i in range(1, 101):
         x = 0
 
 
-while True:
 
+
+collides_with = None
+
+while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                for n, ship in enumerate(ships):
+                    if ship.body.collidepoint(event.pos):
+                        collides_with = n
+
+        if event.type == pygame.MOUSEBUTTONUP:
+            if event.button == 1:
+                collides_with = None
+
+        if event.type == pygame.MOUSEMOTION:
+            if collides_with != None:
+                ships[collides_with].body.move_ip(event.rel)
+                
+                
         SCREEN.fill((98, 99, 181))
         for ob in enemy_tiles:
             ob.draw()
@@ -80,6 +118,11 @@ while True:
         for ob in your_tiles:
             ob.draw()
 
+        for s in ships:
+            s.draw()
+
+
 
     pygame.display.flip()
     CLOCK.tick(FPS)        
+
