60c24da1624fc846f8b708500e5374a0ac42b3e8
sandie <urbanskaaleksandra001@gmail.com>
2025-12-02 23:11:36 +0100

    dodanie planu funkcji pozostalych do napisania i stworzenie poczatkowej struktury funkcji: podejmujacej decyzje, losujacej cel i szukajacej dalszej czesci statku

--- bot_statkowy.py
+++ bot_statkowy.py
@@ -1,7 +1,14 @@
+import random
 plansza=[[0]*10 for i in range(10)]
 original=[[0]*10 for i in range(10)]
 pozostale_statki=[0, 4, 3, 2, 1] #indeks to dlugosc statku
-ostatni_trafiony
+sprawdz_obok_x=[1,-1,0,0]
+sprawdz_obok_y=[0,0,1,-1]
+ktory=0
+ostatni_trafiony=(None, None)
+wspolrzedne_poczatku=(None, None)
+statek_na_celowniku=0
+ktory_z_rzedu=0
 def zaktualizuj_pozostale_pola():
 	pozostale=[]
 	for i in range (10):
@@ -9,17 +16,41 @@ def zaktualizuj_pozostale_pola():
 			if plansza[i][j]!=-1:
 				pozostale.append((i,j))
 	return pozostale
-def strzelaj():
-	losuj=zaktualizuj_pozostale_pola()
-	strzal=random.choice(losuj)
+def czy_zostaly_statki(od): #sprawdz czy jest sens dalej szukac reszty statku
+    for i in range (od, 5):
+        if(pozostale_statki[i]):
+            return True
+    return False  
+def zatopiony():  #zaznacz pola wokoło zatopionego statku
+def losuj(losowanie):
+	strzal=random.choice(losowanie)
 	i, j=strzal
-	plansza[i][j]=-1
-	while original[i][j]==1:
-		ostatni_trafiony=(i, j)
-		losuj=zaktualizuj_pozostale_pola()
-		if pozostale_statki[1]!=0 or pozostale_statki[2]!=0 or pozostale_statki[3]!=0 or pozostale_statki[4]!=0:
-			strzal=
+	return i,j
+def kontynuuj(i,j): #jesli trafiony strzelaj dalej
+        global ktory, ktory_z_rzedu
+		if ktory_z_rzedu==1:
+		    while ktory<4:
+		        test_i=i+sprawdz_obok_x[ktory]
+		        test_j=j+sprawdz_obok_y[ktory]
+		        ktory+=1
+		        if test_i>-1 and test_j>-1 and test_i<10 and test_j<10 and plansza[test_i][test_j]!=-1:
+		            return test_i, test_j
+		    return (None, None)
 		else:
-			strzal=random.choice(losuj)	
-		i, j=strzal
-		plansza[i][j]=-1
+		    #do pomyslenia
+def strzelaj():
+    global statek_na_celowniku, ostatni_trafiony, ktory_z_rzedu, ktory
+    losowanie=zaktualizuj_pozostale_pola()
+    i,j=(None, None)
+	if statek_na_celowniku and ostatni_trafiony is not None and czy_zostaly_statki(ktory_z_rzedu):
+	    i,j=kontynuuj(ostatni_trafiony[0],ostatni_trafiony[1])
+	if not statek_na_celowniku or i is None or not czy_zostaly_statki(ktory_z_rzedu):
+        statek_na_celowniku=0
+        ktory_z_rzedu=0
+        ktory=0
+	    i,j=losuj(losowanie)
+	plansza[i][j]=-1
+	if original[i][j]==1:
+	    ktory_z_rzedu+=1
+	    statek_na_celowniku=1
+	    ostatni_trafiony=(i,j)
