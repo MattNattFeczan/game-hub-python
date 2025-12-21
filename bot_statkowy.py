f80ef6179c23e22248c3738723e52c064777bcd5
sandie <urbanskaaleksandra001@gmail.com>
2025-12-21 14:09:52 +0100

    poprawienie logiki zatapiania statkow

--- bot_statkowy.py
+++ bot_statkowy.py
@@ -93,6 +93,41 @@ def czy_zostaly_statki(od): #sprawdz czy jest sens dalej szukac reszty statku
         if(pozostale_statki[i]):
             return True
     return False  
+    
+def zatop(statek):
+    x_pocz, y_pocz=statek[0]
+    x_kon, y_kon=statek[-1]
+    for i in range(x_pocz-1, x_kon+2):
+        for j in range(y_pocz-1, y_kon+2):
+            if i>-1 and i<10 and j>-1 and j<10:
+                if plansza[i][j]!=-2:
+                    plansza[i][j]=-1
+
+def czy_wolne_obok(statek):
+    if len(statek)==1:
+        x=statek[0][0]
+        y=statek[0][1]
+        for i in range (4):
+            test_i=x+sprawdz_obok_x[i]
+            test_j=y+sprawdz_obok_y[i] 
+            if test_i>-1 and test_j>-1 and test_i<10 and test_j<10 and plansza[test_i][test_j]!=-1 and plansza[test_i][test_j]!=-2:
+                return True
+    elif len(statek)>1:
+        x_pocz, y_pocz=statek[0]
+        x_kon, y_kon=statek[-1]
+        if x_pocz-x_kon==0:
+            if y_pocz-1>-1 and plansza[x_pocz][y_pocz-1]!=-1 and plansza[x_pocz][y_pocz-1]!=-2:
+                return True
+            elif y_kon+1<10 and plansza[x_pocz][y_kon+1]!=-1 and plansza[x_pocz][y_kon+1]!=-2:
+                return True
+        elif y_pocz-y_kon==0:
+            if x_pocz-1>-1 and plansza[x_pocz-1][y_pocz]!=-1 and plansza[x_pocz-1][y_pocz]!=-2:
+                return True
+            elif x_kon+1<10 and plansza[x_kon+1][y_pocz]!=-1 and plansza[x_kon+1][y_pocz]!=-2:
+                return True
+    return False
+
+
 def losuj(losowanie):
 	strzal=random.choice(losowanie)
 	i, j=strzal
@@ -107,10 +142,7 @@ def kontynuuj(i,j): #jesli trafiony strzelaj dalej
             ktory+=1
             if test_i>-1 and test_j>-1 and test_i<10 and test_j<10 and plansza[test_i][test_j]!=-1 and plansza[test_i][test_j]!=-2:
                 return test_i, test_j
-        for a in range (-1, 2): #zatop statek
-            for b in range (-1, 2):
-                if i+a>-1 and i+a<10 and j+b>-1 and j+b<10 and plansza[i+a][j+b]!=-2: plansza[i+a][j+b]=-1
-        pozostale_statki[1]-=1
+        print('help')        
         return None, None
     else:
         x_pocz, y_pocz=statek[0]
@@ -120,31 +152,19 @@ def kontynuuj(i,j): #jesli trafiony strzelaj dalej
                 return x_pocz, y_pocz-1
             elif y_kon+1<10 and plansza[x_pocz][y_kon+1]!=-1 and plansza[x_pocz][y_kon+1]!=-2:
                 return x_pocz, y_kon+1
-            for i in range (-1, len(statek)+1): #zatop
-                if y_pocz+i>-1 and y_pocz+i<10:
-                    if x_pocz-1>-1:
-                        plansza[x_pocz-1][y_pocz+i]=-1
-                    if x_pocz+1<10:
-                        plansza[x_pocz+1][y_pocz+i]=-1
         elif y_pocz-y_kon==0:
             if x_pocz-1>-1 and plansza[x_pocz-1][y_pocz]!=-1 and plansza[x_pocz-1][y_pocz]!=-2:
                 return x_pocz-1, y_pocz
             elif x_kon+1<10 and plansza[x_kon+1][y_pocz]!=-1 and plansza[x_kon+1][y_pocz]!=-2:
                 return x_kon+1, y_pocz
-            for i in range (-1, len(statek)+1): #zatop
-                if x_pocz+i>-1 and x_pocz+i<10:
-                    if y_pocz-1>-1:
-                        plansza[x_pocz+i][y_pocz-1]=-1
-                    if y_pocz+1<10:
-                        plansza[x_pocz+i][y_pocz+1]=-1
-        pozostale_statki[len(statek)]-=1
+        print('pomocy')
         return None, None
 
 def strzelaj():
     global statek, ktory, trafione_pola
     losowanie=zaktualizuj_pozostale_pola()
     i,j=(None, None)
-    if statek and czy_zostaly_statki(len(statek)):
+    if statek:
         i,j=kontynuuj(statek[-1][0], statek[-1][1])
     if i is None: #jesli zamiast if wstawic else nie sprawdzalby czy kontynuuj zwrocilo none
         ktory=0
@@ -152,15 +172,31 @@ def strzelaj():
         i,j=losuj(losowanie)
     plansza[i][j]=-1
     if original[i][j]==1:
-        if plansza[i][j]==-2: print('pomocy')
         trafione_pola+=1
         if trafione_pola==20:
             #print('zwyciestwo')
             return 2
         plansza[i][j]=-2
         bisect.insort(statek, (i, j))
+        if not czy_zostaly_statki(len(statek)+1):
+            zatop(statek)
+            print('brak dluzszych statkow')
+            pozostale_statki[len(statek)]-=1
+            statek=[]
+        elif statek and not czy_wolne_obok(statek):
+            zatop(statek)
+            print('brak wolnych miejsc')
+            pozostale_statki[len(statek)]-=1
+            statek=[]
         return 1
     else:
+        #sprawdz czy jest sens celowac dalej czy trzeba zatopic statek
+        if statek and not czy_wolne_obok(statek):
+            zatop(statek)
+            print('brak wolnych miejsc')
+            pozostale_statki[len(statek)]-=1
+            statek=[]
+        plansza[i][j]=-1
         return 0
        
 ustaw_statki()
@@ -174,10 +210,10 @@ for i in range (100):
     if strzelaj()==2:
         print(i)
         exit()
-    #for j in range(10):
-     #   for k in range(10):
-      #      if plansza[j][k]==-2: print("S",end=" ")
-       #     if plansza[j][k]==-1: print("X",end=" ")
-        #    if plansza[j][k]==0: print("~",end=" ")
-        #print('\n')
-    #print(pozostale_statki[1], ' ', pozostale_statki[2], ' ', pozostale_statki[3], ' ', pozostale_statki[4], ' ')     
+    for j in range(10):
+        for k in range(10):
+            if plansza[j][k]==-2: print("S",end=" ")
+            if plansza[j][k]==-1: print("X",end=" ")
+            if plansza[j][k]==0: print("~",end=" ")
+        print('\n')
+    print(pozostale_statki[1], ' ', pozostale_statki[2], ' ', pozostale_statki[3], ' ', pozostale_statki[4], ' ')     
